ad98f294ada925dc43e5a971ce824aa7c3c98315
chenzhaolong <1332588507@qq.com>
2020-05-15 01:23:34 +0800

    feat：rn模式下h5-side的api

---
+++ interface/h5-side.ts
@@ -0,0 +1,12 @@
+/**
+ * @file h5端的jsBridge的api类型
+ */
+
+export namespace H5Side {
+    // rn-side的api格式
+    export type ApiMap = {[key: string]: (params: any, send: (data: any) => any) => any}
+
+    export type invoke = (data?: any) => any
+
+    export type listen = (data?: any) => any
+}
