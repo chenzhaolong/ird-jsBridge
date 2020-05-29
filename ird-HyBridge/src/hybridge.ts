0253664f99c56968e5af5f8be093da3686a28144
chenzhaolong <1332588507@qq.com>
2020-05-30 01:31:18 +0800

    搭建ird-HyBridge

---
+++ ird-HyBridge/src/hybridge.ts
@@ -0,0 +1,27 @@
+/**
+ * @file 主体部分
+ */
+
+export const _Hybridge = (function() {
+    // 回调ID
+    let cbId: number = 0;
+
+    // 临时队列
+    let tmpQueue: Array<any> = [];
+
+    // 验证成功后回传的token
+    let NaToken: string = '';
+
+    return {
+        init() {},
+        register() {},
+        listen() {},
+        emit() {},
+        invoke() {},
+        getInfo() {},
+        extends() {},
+        error() {},
+        debug() {},
+        invokeByNative() {}
+    }
+})();
--- ird-HyBridge/src/index.ts
+++ ird-HyBridge/src/index.ts
@@ -1,22 +1,13 @@
 /**
  * @file 入口文件
  */
+import { _Hybridge } from "./hybridge";
 
 export const HyBridge = (function() {
     // @ts-ignore
     if (!window.HyBridge || Object.keys(window.HyBridge).length === 0) {
         // @ts-ignore
-        window.HyBridge = {
-            init() {},
-            register() {},
-            listen() {},
-            emit() {},
-            invoke() {},
-            getInfo() {},
-            extends() {},
-            error() {},
-            debug() {}
-        };
+        window.HyBridge = _Hybridge;
     }
     // @ts-ignore
     return window.HyBridge;
