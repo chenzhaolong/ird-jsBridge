240ffdde04a37dc1f814ea7526bb8c842d50a652
chenzhaolong <1332588507@qq.com>
2020-07-24 01:45:45 +0800

    feat：开发sessionStore的store的操作

--- ird-RnBridge/utils/store.ts
+++ ird-RnBridge/utils/store.ts
@@ -3,28 +3,45 @@
  */
 
 class Store {
+    private cache: {[key: string]: any};
+
     constructor() {
-
+        this.cache = {};
     }
 
-    add() {
-
+    add(key: string, data: any): boolean {
+        if (this.cache[key]) {
+            console.warn(`the key ${key} is exist in the store, please change the type when use sessionStore.`);
+            return false;
+        }
+        this.cache[key] = data;
+        return true;
     }
 
-    del() {
-
+    del(key: string): boolean {
+        if (this.cache[key]) {
+            this.cache[key] = null;
+            return delete this.cache[key];
+        }
+        console.warn(`the key ${key} has been delete yet.`);
+        return true;
     }
 
-    modify() {
-
+    modify(key: string, data: any): boolean {
+        if (this.cache[key]) {
+            this.cache[key] = data;
+            return true;
+        }
+        console.warn(`the key ${key} has not existed in the store, so can not modify.`);
+        return false;
     }
 
-    get(key: string) {
-        return {}
+    get(key: string): any {
+        return this.cache[key];
     }
 
     clear() {
-
+        this.cache = {};
     }
 }
 
